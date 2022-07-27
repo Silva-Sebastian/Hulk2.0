7c26f9d6704ca4cdaafc5c64584530707abdde24
Silva-Sebastian <sebasps1991@gmail.com>
2022-07-27 19:33:30 -0300

    agregando js

---
+++ js/scrip.js
@@ -0,0 +1,68 @@
+let nombre = prompt('ingresa tu nombre: ')
+console.log(nombre)
+
+let edad = prompt('ingresa tu edad: ')
+console.log(edad)
+
+alert('bienvenido ' + nombre + ' tu edad es ' + edad)
+
+let precioT = 0
+
+
+if (edad >= 18) {
+    alert('podes inscribirte')
+    let cantClases = prompt('cuantas clases te interesan')
+
+
+    for (let i = 0; i < cantClases; i++){
+
+        let clase = prompt('a que clase te queres sumar: \n- k\n- f ')
+        let dias = parseInt (prompt('cuantos dias queres entrenar:'))
+        let precioxdia = 2500;
+
+        if (clase == 'k'){
+            precioT += dias * precioxdia
+            let precio1 = alert('el valor seria de $' + (dias * precioxdia))
+        }else if (clase == 'f'){
+            precioT += dias * precioxdia
+            let precio2 = alert('el valor seria de $' + (dias * precioxdia))
+        
+        } else {
+            alert('no hay mas clases disponibles')
+        }
+    }
+
+} else {
+    alert('necesitas autorizacion')
+}
+
+function desc(precio){
+    return precio * 0.9
+}
+
+let precioEfectivo = desc(precioT)
+
+
+alert('si pagas en efectivo el total seria de $' + precioEfectivo)
+alert('si pagas con tarjeta el total seria de $' + precioT)
+
+let num = parseInt(prompt ('pone tu numero de cliente'))
+
+class cliente {
+    constructor(nombre, edad){
+        this.nombre = nombre.toUpperCase();
+        this.edad = parseFloat(edad);
+        this.num = parseFloat(num)
+    }
+}
+
+const clientes = [];
+const id = []
+
+clientes.push(new cliente(nuevo, edad, num));
+
+
+for (const refe of clientes){
+    console.log(refe.nombre)
+    console.log(refe.num)
+}
